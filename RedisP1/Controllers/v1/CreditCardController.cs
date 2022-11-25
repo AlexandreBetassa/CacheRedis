aba8853062cf3ba46d52df0b605b69cb0ac924cf
Alexandre Betassa <alexandre.betassa.souza@hotmail.com>
2022-11-25 09:34:18 -0300

    testado codigo

--- RedisP1/Controllers/v1/CreditCardController.cs
+++ RedisP1/Controllers/v1/CreditCardController.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Distributed;
-using RedisP1.Contracts.v1;
 using RedisP1.Models.v1;
 using RedisP1.Services.Contracts.v1;
 using System.Text.Json;
@@ -12,56 +11,29 @@ namespace RedisP1.Controllers.v1
     public class CreditCardController : ControllerBase
     {
         private readonly IService<CreditCard> _service;
-        private readonly IDistributedCache _cache;
 
-        public CreditCardController(IService<CreditCard> service, IDistributedCache cache)
+        public CreditCardController(IService<CreditCard> service)
         {
             _service = service;
-            _cache = cache;
         }
 
         [HttpPost]
         public async Task<ActionResult<CreditCard>> Create(CreditCard entity)
         {
             await _service.Create(entity);
-            await _cache.RemoveAsync("CreditsCard");
             return CreatedAtRoute("GetOneCreditCard", new { id = entity.Id }, entity);
         }
 
         [HttpGet]
         public async Task<ActionResult<List<CreditCard>>> Get()
         {
-            var creditsJson = await _cache.GetStringAsync("CreditsCard");
-            if (creditsJson != null)
-            {
-                var credits = JsonSerializer.Deserialize<List<CreditCard>>(creditsJson);
-                return credits;
-            }
-            else
-            {
-                var credits = await _service.GetAll();
-                string jsonCredits = JsonSerializer.Serialize<List<CreditCard>>(credits.Value);
-                await _cache.SetStringAsync("CreditsCard", jsonCredits);
-                return credits;
-            }
+            return await _service.GetAll();
         }
 
         [HttpGet("{id}", Name = "GetOneCreditCard")]
         public async Task<ActionResult<CreditCard>> Get(string id)
         {
-            var creditsJson = await _cache.GetStringAsync("CreditsCard");
-            if (creditsJson != null)
-            {
-                var credits = JsonSerializer.Deserialize<List<CreditCard>>(creditsJson);
-                return credits.Where(creditCard => creditCard.Id == id).FirstOrDefault();
-            }
-            else
-            {
-                var credits = await _service.Get(id);
-                string jsonCredits = JsonSerializer.Serialize<CreditCard>(credits.Value);
-                await _cache.SetStringAsync("CreditsCard", jsonCredits);
-                return credits;
-            }
+            return await _service.Get(id);
         }
     }
 }
--- RedisP1/Database/v1/Database.cs
+++ RedisP1/Database/v1/Database.cs
@@ -1,32 +1,62 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Distributed;
 using RedisP1.Contracts.v1;
+using RedisP1.Models.v1;
+using System.Text.Json;
 
 namespace RedisP1.Database.v1
 {
     public class Database<T> : IDatabase<T> where T : class
     {
         private readonly AppDbContext _db;
-        public Database(AppDbContext db)
+        private readonly IDistributedCache _cache;
+        public Database(AppDbContext db, IDistributedCache cache)
         {
             _db = db;
+            _cache = cache;
         }
 
         public async Task<T> CreateAsync(T entity)
         {
             await _db.Set<T>().AddAsync(entity);
             await _db.SaveChangesAsync();
+            await _cache.RemoveAsync(typeof(T).Name);
             return await Task.FromResult(entity);
         }
 
         public async Task<List<T>> GetAllAsync()
         {
-            return await _db.Set<T>().ToListAsync();
+
+            var entityJson = await _cache.GetStringAsync(typeof(T).Name);
+            if (entityJson != null)
+            {
+                var entity = JsonSerializer.Deserialize<List<T>>(entityJson);
+                return entity;
+            }
+            else
+            {
+                var entity = _db.Set<T>().ToList();
+                string jsonEntity = JsonSerializer.Serialize(entity);
+                await _cache.SetStringAsync(typeof(T).Name, jsonEntity);
+                return entity;
+            }
         }
 
         public async Task<T> GetAsync(string id)
         {
-            return await _db.Set<T>().FindAsync(id);
+            var entity = await _cache.GetStringAsync(typeof(T).Name + id);
+            if (entity != null)
+            {
+                return JsonSerializer.Deserialize<T>(entity);
+            }
+            else
+            {
+                var entityObj = await _db.Set<T>().FindAsync(id);
+                var entityJson = JsonSerializer.Serialize<T>(entityObj);
+                await _cache.SetStringAsync(typeof(T).Name + id, entityJson);
+                return entityObj;
+            }
         }
     }
 }
